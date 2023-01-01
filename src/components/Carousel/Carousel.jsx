cb37bcb4ad835462eb936606ff5d2bc14e94268e
shrinivas Joshi appdeployer <shrinivas@appsdeployer.com>
2023-01-01 17:37:42 +0530

    carousel button changes

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -24,11 +24,11 @@ const Carousel = () => {
           </div>
 
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="carousel-control-prev-icon" aria-hidden="false"></span>
             <span className="visually-hidden">Previous</span>
           </button>
           <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="carousel-control-next-icon" aria-hidden="false"></span>
             <span className="visually-hidden">Next</span>
           </button>
 
